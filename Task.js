d531c12a9588758b37e70861b104abef99538117
Piyush Dave <piyushdave2023@gmail.com>
2025-05-23 14:00:47 +0530

    comment line

--- Task.js
+++ Task.js
@@ -3,6 +3,8 @@ const target = -2;
 
 
 function equalsum(arr, target){
+    // we can use arrow funnction also  
+    //  const equalsum = (arr, target)=>{}
     for (let i = 0; i < arr.length; i++){
         for(let j = i + 1; j < arr.length; j++){
             if (arr[i] + arr[j] === target){
